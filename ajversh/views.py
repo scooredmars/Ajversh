96723c5ef05d7d57c75c6fba64418d6f00f18dc5
scooredmars <olo880@spoko.pl>
2020-12-16 02:24:41 +0100

    basic views and urls configuration

--- ajversh/views.py
+++ ajversh/views.py
@@ -2,3 +2,27 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, "home.html")
+
+def sets_list_view(request):
+    return render(request, "user/sets.html")
+
+def guild_view(request):
+    return render(request, "user/guild.html")
+
+def sets_creator_view(request):
+    return render(request, "user/creator.html")
+
+def dc_settings_view(request):
+    return render(request, "user/dc-settings.html")
+
+def profile_view(request):
+    return render(request, "user/profile.html")
+
+def edit_view(request):
+    return render(request, "user/edit-profile.html")
+
+def messages_view(request):
+    return render(request, "user/messages.html")
+
+def logout_view(request):
+    pass
